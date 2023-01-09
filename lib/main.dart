415da2d66214584cafb4ea40916e0009fccf3f7d
Harish K <104665179+kharishgit@users.noreply.github.com>
2023-01-09 20:05:57 +0530

    Secnd Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main(){
+// 
+void main()=>runApp(MyApp());
 
-}
 class MyApp extends StatelessWidget {
-  build(){
+  get raisedButtonStyle => null;
+  void AnswerQuestion(){
+    print('Answer is Passed');
+  }
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(
+        title: Text('SecApp'),
+      ), 
+      body:Column(
+        children: [
+        Text("The question is"),
+        ElevatedButton(
+              style: raisedButtonStyle,
+              onPressed: AnswerQuestion,
+              child: Text('Answer1'),
+            ),
+        ElevatedButton(
+              style: raisedButtonStyle,
+              onPressed: (){
+                print('Answer 2 Choosen ');
+              },
+              child: Text('Answer1'),
+            ),
+        ElevatedButton(
+              style: raisedButtonStyle,
+              onPressed: AnswerQuestion,
+              child: Text('Answer1'),
+            ) 
+        ],
+      ),
+      ),
+      );
     
   }
 }
